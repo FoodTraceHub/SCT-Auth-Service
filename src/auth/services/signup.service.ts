5444d05fe73a1bb51b261433da7fb593213f2b8b
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2024-01-20 22:43:11 +0100

    feat: password hashing

--- src/auth/services/signup.service.ts
+++ src/auth/services/signup.service.ts
@@ -3,41 +3,51 @@ import { BadRequestError, ConflictError, ErrorResponse, SuccessResponse, IContro
 import { StatusCodes } from "http-status-codes";
 import { Op } from "sequelize";
 import { User } from "../model";
+import { hashPassword } from "../../core";
 
 export class SignUpService {
     constructor(
         private readonly _user: typeof User
     ) {}
 
-    handle = async ({input} : IControllerArgs<AuthSignUpDto>) => {
-
-        if (!input) {
-            throw new BadRequestError('Input is required');
-        }
-
-        const { email, password, name, role } = input;
-
-        // check if user already exists
-        const user = await this._user.findOne({
-            where: {
-                email: {
-                    [Op.eq]: email
-                }
+    handle = async ({ input }: IControllerArgs<AuthSignUpDto>) => {
+        try {
+            if (!input) {
+                throw new BadRequestError('Input is required');
             }
-        });
 
-        if (user) {
-            throw new ConflictError('User already exists');
+            const { email, password, name, role } = input;
+
+            // check if user already exists
+            const user = await this._user.findOne({
+                where: {
+                    email: {
+                        [Op.eq]: email
+                    }
+                }
+            });
+
+            if (user) {
+                throw new ConflictError('User already exists');
+            }
+
+            // hash password
+            const hashedPassword = await hashPassword(password);
+
+            // create user
+            const newUser = await this._user.create({
+                email,
+                password: hashedPassword,
+                name,
+                role
+            });
+
+            return SuccessResponse.create(StatusCodes.CREATED, newUser, 'User created successfully');
+        } catch (error) {
+            // handle error here
+            if (error instanceof ErrorResponse) {
+                throw error;
+            }
         }
-
-        // create user
-        const newUser = await this._user.create({
-            email,
-            password,
-            name,
-            role
-        });
-
-        return SuccessResponse.create(StatusCodes.CREATED, newUser, 'User created successfully');
-}
+    }
 }
--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,1 +1,2 @@
 export * from "./parseControllerArgs";
+export * from './hashing';
