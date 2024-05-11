3c81acc783f36f0ae4357f66bf0e5a4e9ee55097
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2024-05-11 13:42:02 +0100

    chore: Remove unused error classes and update folder structure

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,4 @@
 export * from './config';
-export * from '../errors';
 export * from './types';
 export * from '../utils/response';
 export * from '../utils';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,5 @@
-export * from "./parseControllerArgs";
 export * from './hashing';
+export * from './errors';
+export * from './response';
+export * from './apiError';
+export * from './validate';
