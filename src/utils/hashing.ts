61ed6907d645bbc722f2edba0ebc6e0d86a2ea76
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2024-05-13 18:42:08 +0100

    Refactor hashing utility to use double quotes for import statements

--- src/utils/hashing.ts
+++ src/utils/hashing.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10);
@@ -11,16 +11,3 @@ export const comparePassword = async (
 ): Promise<boolean> => {
   return bcrypt.compare(password, hash);
 };
-
-export const hashPasswordSync = (password: string): string => {
-  const salt = bcrypt.genSaltSync(10);
-  return bcrypt.hashSync(password, salt);
-};
-
-export const comparePasswordSync = (
-  password: string,
-  hash: string
-): boolean => {
-  return bcrypt.compareSync(password, hash);
-};
-
